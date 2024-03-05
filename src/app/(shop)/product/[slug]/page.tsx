8bd05f12167795ca08a7c6401d4d0c2df88b1e23
nachowolf11 <ignaciowolf10@gmail.com>
2024-03-04 21:09:54 -0300

    Metadata

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -5,6 +5,7 @@ import { notFound } from 'next/navigation';
 import { titleFont } from '@/config/fonts';
 import { ProductMobileSlideshow, ProductSlideshow, QuantitySelector, SizeSelector, StockLabel } from '@/components';
 import { getProductBySlug } from '@/actions';
+import { Metadata, ResolvingMetadata } from 'next';
 
 interface Props {
   params: {
@@ -12,7 +13,29 @@ interface Props {
   };
 }
 
-
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const slug = params.slug
+ 
+  // fetch data
+  const product = await getProductBySlug(slug);
+ 
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent).openGraph?.images || []
+ 
+  return {
+    title: product?.title ?? 'Producto no encontrado',
+    description: product?.description ?? '',
+    openGraph: {
+      title: product?.title ?? 'Producto no encontrado',
+      description: product?.description ?? '',
+      images: [`/products/${ product?.images[1] }`],
+    },
+  }
+}
 
 export default async function ProductPage( { params }: Props ) {
 
