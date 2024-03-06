c59388c9a4c4b2448a6563e02dad64bac8ec38c9
nachowolf11 <ignaciowolf10@gmail.com>
2024-03-06 20:22:04 -0300

    CartStore

--- src/app/(shop)/product/[slug]/ui/AddToCart.tsx
+++ src/app/(shop)/product/[slug]/ui/AddToCart.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import { QuantitySelector, SizeSelector } from "@/components"
-import type { Product, Size } from "@/interfaces"
+import type { CartProduct, Product, Size } from "@/interfaces"
+import { useCartStore } from "@/store"
 import { useState } from "react"
 
 interface Props {
@@ -9,6 +10,8 @@ interface Props {
 }
 
 export const AddToCart = ({ product }:Props) => {
+
+    const addProductToCart = useCartStore( state => state.addProductToCart );
 
     const [size, setSize] = useState<Size|undefined>();
     const [quantity, setQuantity] = useState<number>(1);
@@ -20,7 +23,21 @@ export const AddToCart = ({ product }:Props) => {
             return;
         }
 
-        console.log({ size, quantity });
+        const cartProduct: CartProduct = {
+            id: product.id,
+            slug: product.slug,
+            title: product.title,
+            price: product.price,
+            quantity: quantity,
+            size: size,
+            image: product.images[0]
+        }
+
+        addProductToCart(cartProduct);
+
+        setPosted(false);
+        setQuantity(1);
+        setSize(undefined);
     }
 
     return (
