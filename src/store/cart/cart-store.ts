d1adb41b39911ca37aadf7ff8da55bf65beee4dc
nachowolf11 <ignaciowolf10@gmail.com>
2024-03-11 14:08:42 -0300

    Counter cart

--- src/store/cart/cart-store.ts
+++ src/store/cart/cart-store.ts
@@ -5,36 +5,54 @@ import { persist } from "zustand/middleware";
 interface State {
     cart: CartProduct[];
 
-    addProductToCart: ( product: CartProduct ) => void;
+
+    getTotalItems: () => number;
+
+    addProductToCart: (product: CartProduct) => void;
 }
 
 export const useCartStore = create<State>()(
-    
-    (set, get) => ({
 
-    cart: [],
-    
-    addProductToCart: ( product: CartProduct ) => {
-        const { cart } = get();
+    persist(
+        (set, get) => ({
 
-        const productInCart = cart.some(
-            (item) => item.id === product.id && item.size === product.size
-        );
+            cart: [],
 
-        if ( !productInCart ) {
-            set({ cart: [...cart, product] })
-            return;
-        }
+            getTotalItems: () => {
+                const { cart } = get();
+                return cart.reduce( ( total, item ) => total + item.quantity, 0 );
+            },
 
-        const updatedCartProducts = cart.map( (item) => {
+            addProductToCart: (product: CartProduct) => {
 
-            if ( item.id === product.id && item.size === product.size ) {
-                return { ...item, quantity: item.quantity + product.quantity }
+                persist
+
+                const { cart } = get();
+
+                const productInCart = cart.some(
+                    (item) => item.id === product.id && item.size === product.size
+                );
+
+                if (!productInCart) {
+                    set({ cart: [...cart, product] })
+                    return;
+                }
+
+                const updatedCartProducts = cart.map((item) => {
+
+                    if (item.id === product.id && item.size === product.size) {
+                        return { ...item, quantity: item.quantity + product.quantity }
+                    }
+
+                    return item;
+                })
+
+                set({ cart: updatedCartProducts });
             }
+        }),
+        {
+            name: 'shopping-cart'
+        }
+    )
 
-            return item;
-        })
-
-        set({ cart: updatedCartProducts });
-    }
-  }));
+);
