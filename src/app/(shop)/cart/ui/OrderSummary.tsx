7ce3c3f60297b63dc1d113d519a98982853f6fb5
nachowolf11 <ignaciowolf10@gmail.com>
2024-03-12 18:47:49 -0300

    Summary order

---
+++ src/app/(shop)/cart/ui/OrderSummary.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import { useCartStore } from "@/store";
+import { useEffect, useState } from "react"
+import { currencyFormat } from '@/utils';
+import { useRouter } from "next/navigation";
+
+export const OrderSummary = () => {
+
+    const router = useRouter();
+
+    const [loaded, setLoaded] = useState(false);
+    const { total, subTotal, tax, itemsInCart } = useCartStore( state => state.getSummaryInformation() );
+
+    useEffect(() => {
+        setLoaded(true);
+      }, []);    
+
+    useEffect(() => {
+
+        if ( itemsInCart === 0 && loaded === true )   {
+          router.replace('/empty')
+        }
+    
+    
+      },[ itemsInCart, loaded ])
+
+    if ( !loaded ) return <p>Loading...</p> 
+    
+
+    return (
+        <div className="grid grid-cols-2">
+
+            <span>No. Productos</span>
+            <span className="text-right">{ itemsInCart === 1 ? '1 artículo' : `${itemsInCart} artículos` }</span>
+
+            <span>Subtotal</span>
+            <span className="text-right">{ currencyFormat(subTotal) }</span>
+
+            <span>Impuestos (15%)</span>
+            <span className="text-right">{ currencyFormat(tax) }</span>
+
+            <span className="mt-5 text-2xl">Total:</span>
+            <span className="mt-5 text-2xl text-right">{ currencyFormat(total) }</span>
+
+
+        </div>
+    )
+}
--- src/store/cart/cart-store.ts
+++ src/store/cart/cart-store.ts
@@ -7,8 +7,16 @@ interface State {
 
 
     getTotalItems: () => number;
+    getSummaryInformation: () => {
+        subTotal: number;
+        tax: number;
+        total: number;
+        itemsInCart: number;
+    }
 
     addProductToCart: (product: CartProduct) => void;
+    updateProductQuantity: (product: CartProduct, quantity: number) => void;
+    removeProduct: (product: CartProduct) => void;
 }
 
 export const useCartStore = create<State>()(
@@ -20,12 +28,26 @@ export const useCartStore = create<State>()(
 
             getTotalItems: () => {
                 const { cart } = get();
-                return cart.reduce( ( total, item ) => total + item.quantity, 0 );
+                return cart.reduce((total, item) => total + item.quantity, 0);
+            },
+            getSummaryInformation: () => {
+                const { cart } = get();
+
+                const subTotal = cart.reduce(
+                    (subTotal, product) => (product.quantity * product.price) + subTotal,
+                    0
+                );
+                const tax = subTotal * 0.15;
+                const total = subTotal + tax;
+                const itemsInCart = cart.reduce((total, item) => total + item.quantity, 0);
+
+                return {
+                    subTotal, tax, total, itemsInCart
+                }
             },
 
-            addProductToCart: (product: CartProduct) => {
 
-                persist
+            addProductToCart: (product: CartProduct) => {
 
                 const { cart } = get();
 
@@ -46,6 +68,29 @@ export const useCartStore = create<State>()(
 
                     return item;
                 })
+
+                set({ cart: updatedCartProducts });
+            },
+
+            updateProductQuantity: (product: CartProduct, quantity: number) => {
+                const { cart } = get();
+
+                const updatedCartProducts = cart.map((item) => {
+
+                    if (item.id === product.id && item.size === product.size) {
+                        return { ...item, quantity: quantity }
+                    }
+
+                    return item;
+                })
+
+                set({ cart: updatedCartProducts });
+
+            },
+            removeProduct: (product: CartProduct) => {
+                const { cart } = get();
+
+                const updatedCartProducts = cart.filter((item) => item.size !== product.size || item.id !== product.id)
 
                 set({ cart: updatedCartProducts });
             }
