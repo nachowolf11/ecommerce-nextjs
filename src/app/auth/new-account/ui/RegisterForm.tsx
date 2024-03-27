3d6af0dd81dcb627d9992a023f28b946818ce996
nachowolf11 <ignaciowolf10@gmail.com>
2024-03-27 19:55:43 -0300

    Register

---
+++ src/app/auth/new-account/ui/RegisterForm.tsx
@@ -0,0 +1,105 @@
+'use client'
+
+import { registerUser } from "@/actions";
+import clsx from "clsx";
+import Link from "next/link"
+import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type FormInputs = {
+    name: string;
+    email: string;
+    password: string;
+}
+
+export const RegisterForm = () => {
+
+    const [errorMessage, setErrorMessage] = useState('');
+    const { register, handleSubmit, formState: {errors} } = useForm<FormInputs>();
+
+    const onSubmit: SubmitHandler<FormInputs> = async (data) => {
+
+        const { name, email, password } = data;
+        const resp = await registerUser( name, email, password );
+
+        if ( !resp.ok ) {
+            setErrorMessage(resp.message);
+            return;
+        }
+        
+        // Server action
+    }
+
+    return (
+        <form onSubmit={ handleSubmit( onSubmit ) } className="flex flex-col">
+
+            <span className="text-red-500">{ errorMessage }</span>
+
+            <label htmlFor="email">Nombre completo</label>
+            <input
+                className={
+                    clsx(
+                        "px-5 py-2 border bg-gray-200 rounded mb-5",
+                        {
+                            'border-red-500': !!errors.name
+                        }
+                    )
+                }
+                type="text"
+                autoFocus
+                {...register('name', { required: true })}
+            />
+
+
+            <label htmlFor="email">Correo electrónico</label>
+            <input
+                className={
+                    clsx(
+                        "px-5 py-2 border bg-gray-200 rounded mb-5",
+                        {
+                            'border-red-500': !!errors.email
+                        }
+                    )
+                }
+                type="email"
+                {...register('email', { required: true, pattern: /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/ })}
+            />
+
+
+            <label htmlFor="password">Contraseña</label>
+            <input
+                className={
+                    clsx(
+                        "px-5 py-2 border bg-gray-200 rounded mb-5",
+                        {
+                            'border-red-500': !!errors.password
+                        }
+                    )
+                }
+                type="password"
+                {...register('password', { required: true, minLength: 6 })}
+            />
+
+            <button
+
+                className="btn-primary">
+                Crear cuenta
+            </button>
+
+
+            {/* divisor l ine */}
+            <div className="flex items-center my-5">
+                <div className="flex-1 border-t border-gray-500"></div>
+                <div className="px-2 text-gray-800">O</div>
+                <div className="flex-1 border-t border-gray-500"></div>
+            </div>
+
+            <Link
+                href="/auth/login"
+                className="btn-secondary text-center">
+                Ingresar
+            </Link>
+
+        </form>
+    )
+}
